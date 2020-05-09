024b78d55de5acb08e44b89d8e957317debbf060
liontwinkle <lion.twinkle.ge@gmail.com>
2020-05-10 01:48:27 +0200

    update the engine

--- git-change.sh
+++
@@ -1,8 +0,0 @@
-git filter-branch -f --env-filter '
-CORRECT_NAME="superlancer9015"
-CORRECT_EMAIL="albert.belotti9315@gmail.com"
-export GIT_COMMITTER_NAME="$CORRECT_NAME"
-export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
-export GIT_AUTHOR_NAME="$CORRECT_NAME"
-export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
-' --tag-name-filter cat -- --branches --tags
